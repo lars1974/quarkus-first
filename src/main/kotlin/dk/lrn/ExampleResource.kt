ec0c4e29729b50d72da695c8530f984b77a8ff5e
lars1974 <lars.jensen.74@gmail.com>
2020-05-21 17:16:35 +0200

    first

---
+++ src/main/kotlin/dk/lrn/ExampleResource.kt
@@ -0,0 +1,25 @@
+package dk.lrn
+
+import org.eclipse.microprofile.config.inject.ConfigProperty
+import javax.enterprise.context.ApplicationScoped
+import javax.inject.Inject
+import javax.ws.rs.GET
+import javax.ws.rs.Path
+import javax.ws.rs.Produces
+import javax.ws.rs.core.MediaType
+
+@Path("/hello")
+class ExampleResource(
+        @ConfigProperty(name = "test")
+        val privateKey: String,
+        @ConfigProperty(name = "greeting.message", defaultValue = "PRIT")
+        val message: String
+) {
+
+    val quarkus = "test1"
+
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    fun hello() = quarkus+privateKey+message
+}
---
+++ src/test/kotlin/dk/lrn/NativeExampleResourceIT.kt
@@ -0,0 +1,6 @@
+package dk.lrn
+
+import io.quarkus.test.junit.NativeImageTest
+
+@NativeImageTest
+open class NativeExampleResourceIT : ExampleResourceTest()
